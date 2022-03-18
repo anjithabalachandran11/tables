3a18363242abaf65c2472a41c7ad3c5a8b7c569c
anjithabalachandran11 <anjithabalachandran11@gmail.com>
2022-03-18 17:31:05 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { FormControl,FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'table';
+  formdata=new FormGroup({
+    name : new FormControl({})
+  })
+  
+  constructor(private http:HttpClient) { }
+
+
+  details:any
+  searchname:any
+  display(){
+    this.http.get('http://localhost:3000/display').subscribe((data)=>{
+    this.details=data
+    console.log("data: ",data)
+    })
+  }
+  search(name:any){
+    const data = {
+      name
+    }
+    this.http.post('http://localhost:3000/search',data).subscribe((data)=>{
+    this.details=data
+    })
+  }
 }
